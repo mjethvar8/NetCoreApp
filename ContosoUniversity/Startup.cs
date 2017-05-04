323baa49252df0e98f1b6f5298cb28ec72150216
Mahesh Jethva <mjethvar8@gmail.com>
2017-05-04 16:59:09 -0400

    - added test data

--- ContosoUniversity/Startup.cs
+++ ContosoUniversity/Startup.cs
@@ -37,7 +37,7 @@ namespace ContosoUniversity
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory, SchoolContext context)
         {
             loggerFactory.AddConsole(Configuration.GetSection("Logging"));
             loggerFactory.AddDebug();
@@ -60,6 +60,8 @@ namespace ContosoUniversity
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
+
+            DbInitializer.Intialize(context);
         }
     }
 }
